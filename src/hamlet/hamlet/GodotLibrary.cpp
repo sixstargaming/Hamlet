2810d1351f506a15881dd95649aad061b1041d20
Kelzorz <43789359+Kelzorz@users.noreply.github.com>
2019-03-28 11:41:03 -0500

    C-Linker File
    Also built the dll.

---
+++ src/hamlet/hamlet/GodotLibrary.cpp
@@ -0,0 +1,20 @@
+/* Include all calsses on this file */
+
+// #include <gdnative_api_struct.gen.h>  // Not needed?
+#include <core/Godot.hpp> // Remove once included in root file?
+
+// DO NOT use namesapce godot here
+
+extern "C" void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *o) {
+	godot::Godot::gdnative_init(o);
+}
+
+extern "C" void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_options *o) {
+	godot::Godot::gdnative_terminate(o);
+}
+
+extern "C" void GDN_EXPORT godot_nativescript_init(void *handle) {
+	godot::Godot::nativescript_init(handle);
+
+	/* Register all classes here with godot::register_class<godot::CLASSNAME>(); */
+}
