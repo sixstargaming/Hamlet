1f3b6f24738d61ff6768c1477447bebc8d07e651
Kelzorz <43789359+Kelzorz@users.noreply.github.com>
2019-04-24 11:45:30 -0500

    AI Movement
    Basic random AI movement.

--- src/hamlet/hamlet/Monster.cpp
+++ src/hamlet/hamlet/Monster.cpp
@@ -14,4 +14,33 @@ Monster::~Monster() { return; }
 
 void Monster::_process(float delta) {
 	if (fHealth <= 0) Die();
+	ProcessAI();
+}
+
+void Monster::ProcessAI() {
+	motion = Vector2(0, 0);
+
+	if (!isStunned) {
+		srand(time(NULL));
+		int iMov = rand() % 3;
+		switch (iMov)
+		{
+		case UP:
+			motion = Vector2(SPEED, 0);
+			break;
+		case DOWN:
+			motion = Vector2(-SPEED, 0);
+			break;
+		case LEFT:
+			motion = Vector2(0, -300);
+			break;
+		case RIGHT:
+			motion = Vector2(0, SPEED);
+			break;
+		default:
+			Godot::print("Non-valid AI Movement");
+			break;
+		}
+	}
+	move_and_slide(motion);
 }
--- src/hamlet/hamlet/Monster.h
+++ src/hamlet/hamlet/Monster.h
@@ -3,6 +3,8 @@
 
 #include "Root.h"
 #include "Actor.h"
+#include <stdlib.h>
+#include <time.h>
 #include <KinematicBody2D.hpp>
 #include <Camera2D.hpp>
 #include <Input.hpp>
@@ -11,13 +13,17 @@ namespace godot {
 	class Monster : public Actor {
 		GODOT_CLASS(Monster, Actor)
 	private:
-		// Vector2 motion;
+		Vector2 motion;
+		int SPEED = 300;
+		enum DIRECTON { UP, DOWN, LEFT, RIGHT };
 	public:
 
 		static void _register_methods();
 		void _init();
 		void _process(float delta);
 		//void _ready();
+
+		void ProcessAI();
 
 		Monster();
 		~Monster();
